d45b404de5a74bbff81b8a168c48d42e33fe555d
Lucas Sowul <sowul.lukas@gmail.com>
2024-08-01 21:59:22 +0200

    404 added
    Exceptions working !!

--- src/main/java/com/RepoRequest/controller/RepoController.java
+++ src/main/java/com/RepoRequest/controller/RepoController.java
@@ -6,10 +6,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @AllArgsConstructor
@@ -19,10 +24,18 @@ public class RepoController {
 
     public final RepoService repoService;
 
-    // mapping for a specific URL
-    // UserNotFound exception is a claas responsible for catching null in username ( username not found in DB)
     @GetMapping("/getUser")
-    public RepoDto[] getUser(@RequestParam String username) throws UserNotFound {
+    public RepoDto[] getUser(@RequestParam String username) {
         return repoService.getUser(username);
+    }
+    // Exception handler
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleUserNotFoundException(UserNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("documentation_url", "https://docs.github.com/rest");
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/RepoRequest/service/RepoService.java
+++ src/main/java/com/RepoRequest/service/RepoService.java
@@ -1,6 +1,7 @@
 package com.RepoRequest.service;
 
 import com.RepoRequest.client.RepoClient;
+import com.RepoRequest.controller.UserNotFoundException;
 import com.RepoRequest.model.RepoDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,9 +13,14 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class RepoService {
 
-     private final RepoClient repoClient= new RepoClient();
-     // calling method getUserRepo(String user) we need to provide a parameter "user". Method return his repository
-     public RepoDto[] getUser(String username) {
-         return repoClient.getUserRepo(username);
+    private final RepoClient repoClient;
+
+    // Calling method getUserRepo(String user) we need to provide a parameter "user". Method returns his repository
+    public RepoDto[] getUser(String username) {
+        RepoDto[] repos = repoClient.getUserRepo(username);
+        if (repos == null || repos.length == 0) {
+            throw new UserNotFoundException("User not found: " + username);
+        }
+        return repos;
      }
 }
