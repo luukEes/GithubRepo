04365e0aface52febb12aa345c21269ea70ab5ca
Lucas <122219985+luukEes@users.noreply.github.com>
2024-08-02 09:14:30 +0200

    Update UserNotFoundException.java

--- src/main/java/com/RepoRequest/controller/UserNotFoundException.java
+++ src/main/java/com/RepoRequest/controller/UserNotFoundException.java
@@ -1,5 +1,6 @@
 package com.RepoRequest.controller;
 
+//added message to exceptions
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(String message) {
         super(message);
